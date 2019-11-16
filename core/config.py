8ee5125b56da350f51106f5f2b17314f4c88d200
ylqi76207 <yunlongqi@my.unt.edu>
2019-11-15 18:10:32 -0600

    20191115, modify config

--- core/config.py
+++ core/config.py
@@ -30,8 +30,8 @@ __C.YOLO.STRIDES                = [8, 16, 32]
 __C.YOLO.ANCHOR_PER_SCALE       = 3
 __C.YOLO.IOU_LOSS_THRESH        = 0.5
 __C.YOLO.UPSAMPLE_METHOD        = "resize"
-__C.YOLO.ORIGINAL_WEIGHT        = "./checkpoint/yolov3_coco.ckpt"
-__C.YOLO.DEMO_WEIGHT            = "./checkpoint/yolov3_coco_demo.ckpt"
+__C.YOLO.ORIGINAL_WEIGHT        = "./checkpoint/original/yolov3_coco.ckpt"
+__C.YOLO.DEMO_WEIGHT            = "./checkpoint/original/yolov3_coco_demo.ckpt"
 
 # Train options
 __C.TRAIN                       = edict()
@@ -45,7 +45,7 @@ __C.TRAIN.LEARN_RATE_END        = 1e-6
 __C.TRAIN.WARMUP_EPOCHS         = 2
 __C.TRAIN.FISRT_STAGE_EPOCHS    = 20
 __C.TRAIN.SECOND_STAGE_EPOCHS   = 30
-__C.TRAIN.INITIAL_WEIGHT        = "./checkpoint/yolov3_coco_demo.ckpt"
+__C.TRAIN.INITIAL_WEIGHT        = "./checkpoint/original/yolov3_coco_demo.ckpt"
 
 
 
@@ -63,9 +63,3 @@ __C.TEST.WEIGHT_FILE            = "./checkpoint/yolov3_test_loss=9.2099.ckpt-5"
 __C.TEST.SHOW_LABEL             = True
 __C.TEST.SCORE_THRESHOLD        = 0.3
 __C.TEST.IOU_THRESHOLD          = 0.45
-
-
-
-
-
-
